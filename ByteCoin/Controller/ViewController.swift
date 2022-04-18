df81b549c6f3c64ca2a9e567539cacf2d8998a37
Sancara <santiagocaraballo@gmail.com>
2022-04-18 07:57:03 -0300

    Fetch data relative to exchange rate

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -45,6 +45,7 @@ extension ViewController: UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(coinManager.currencyArray[row])
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
     }
 }
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,40 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "3710E5A6-0335-4D32-839C-7090F1325CE8"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency: String) {
+        
+        let currencyURL = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(currencyURL: currencyURL)
+        
+    }
+    
+    func performRequest(currencyURL: String) {
+        if let url = URL(string: currencyURL) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) {data, response, error in
+                if error != nil {
+                    //                    delegate?.didFailWithError(self, error: error!)
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    print(String(data: safeData, encoding: .utf8))
+                    
+                    //                    if let change = self.parseJSON(exchangeRate: safeData) {
+                    //                        self.delegate?.didUpdateValue(self, exchangeRate: change)
+                }
+            }
+            task.resume()
+        }
+    }
 }
+
+
+
+
